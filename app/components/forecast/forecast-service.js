01699e3a580d87d1d3e824215e14f6560136ca13
cdelmoral <carderomad@gmail.com>
2015-07-03 13:36:45 -0500

    Refactor forecast service

--- app/components/forecast/forecast-service.js
+++ app/components/forecast/forecast-service.js
@@ -1,33 +1,34 @@
 (function() {
-	'use strict';
+	"use strict";
 
 	angular
-		.module('forecast-service', [])
-		.service('forecastService', ForecastService);
+		.module("forecast-service", [])
+		.service("forecastService", ForecastService);
 
-	ForecastService.$inject = ['$rootScope', '$http'];
+	ForecastService.$inject = ["$rootScope", "$http"];
 
 	var URL_F_BASE = "https://api.forecast.io/forecast/";
 	var FORECAST_KEY = "1c673c349f398fbbbe6ab58f290abefe";
 
 	function ForecastService($rootScope, $http) {
 		var service = this;
+		service.setCoords = setCoords;
 
 		var forecast = {};
 		var location = {};
 
-		service.setCoords = function(lat, lng) {
+		function setCoords(lat, lng) {
 			if (location.latitude != lat || location.longitude != lng) {
 				var forecastUrl = URL_F_BASE + FORECAST_KEY + "/" + lat + "," + lng + "?callback=JSON_CALLBACK";
 				$http.jsonp(forecastUrl).success(function (data) {
 					forecast = data;
-					$rootScope.$broadcast('forecastChanged');
+					$rootScope.$broadcast("forecastChanged");
 				});
 			}
-		};
+		}
 
-		service.getForecast = function() {
+		function getForecast() {
 			return forecast;
-		};
+		}
 	}
 })();
